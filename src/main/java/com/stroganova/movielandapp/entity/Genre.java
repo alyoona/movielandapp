a0872b1143f531bc160714e8a2d6831fead7d9fd
YONA <alenastr12@gmail.com>
2019-07-22 14:48:51 +0300

    [b-3-get-all-genres] fixed after review

--- src/main/java/com/stroganova/movielandapp/entity/Genre.java
+++ src/main/java/com/stroganova/movielandapp/entity/Genre.java
@@ -1,46 +1,12 @@
 package com.stroganova.movielandapp.entity;
 
+
+import lombok.Data;
+
+@Data
 public class Genre {
 
     private long id;
     private String name;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Genre{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Genre)) return false;
-
-        Genre genre = (Genre) o;
-
-        return id == genre.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) (id ^ (id >>> 32));
-    }
 }
--- src/main/java/com/stroganova/movielandapp/web/controller/GenreController.java
+++ src/main/java/com/stroganova/movielandapp/web/controller/GenreController.java
@@ -1,26 +1,32 @@
 package com.stroganova.movielandapp.web.controller;
 
 
+import com.stroganova.movielandapp.entity.Genre;
 import com.stroganova.movielandapp.service.GenreService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/genre")
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal=true, level= AccessLevel.PRIVATE)
 public class GenreController {
 
-    private final GenreService genreService;
-
-    public GenreController(GenreService genreService) {
-        this.genreService = genreService;
-    }
+    @NonNull GenreService genreService;
 
     @GetMapping
-    public ResponseEntity<?> getAll(){
-        return new ResponseEntity<>(genreService.getAll(), HttpStatus.OK);
+    public List<Genre> getAll(){
+        log.info("Get all genres ");
+        return genreService.getAll();
     }
 
 }
